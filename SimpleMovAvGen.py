d224dc37d439d5ea0a0593e6a2aebfb9056a2890
Simamtos <simant@me.com>
2020-05-23 11:47:54 +0200

    Added some description, and modified code so that parameters have their own specific variable

--- SimpleMovAvGen.py
+++ SimpleMovAvGen.py
@@ -1,10 +1,25 @@
+#Simple Random Moving Average Line Chart.
+#Line chart is randomly generated in accordance with  the given desidered lenght of the series (n) and the desidered Max % daily variation  on the the previous  value (daily_upper_range_max_perc and daily_lower_ran).
+#once the line chart has been plotted, three different Simple moving averages are calculated and added to the chart.
+
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+n=500
+daily_upper_range_perc=2
+daily_lower_range_perc=-2
+SMA1=25
+SMA2=50
+SMA3=100
+
+
+#initialise the series with a fiction value of 1 
 random_nbr_series=[1]
-for x in range(1,500):
-	random_nbr_series.append(random_nbr_series[-1]+random_nbr_series[-1]*random.randint(-3,3)/100)
+
+#Generates n length series with thendesaity max daily % variation.
+for x in range(1,n):
+	random_nbr_series.append(random_nbr_series[-1]+random_nbr_series[-1]*random.randint(daily_lower_range_perc,daily_upper_range_perc)/100)
 	
 	
 randomseries=random_nbr_series
@@ -26,8 +41,9 @@ a =sma(randomseries,period)
 b =sma(randomseries,period-30)
 
 plt.figure()
+plt.plot(np.arange(0,len(random_nbr_series),1), random_nbr_series, color='g')
 plt.plot(np.arange(period,len(a)+period,1), a, color='r')
 plt.plot(np.arange(period-30,len(b)+period-30,1), b, color='b')
-plt.plot(np.arange(0,len(random_nbr_series),1), random_nbr_series, color='g')
+
 plt.show()
 
